f2e85aca30d9a2f1dfd8c002220e68e88ea91863
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-03 21:08:15 -0600

    Add Login page

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "../(components)/footer/Footer"
+import LoginForm from "../(components)/login-form/LoginForm"
+
+const page = () => {
+    return (
+        <div className="flex flex-col">
+            <div className="mx-mobilex lg:mx-auto my-[6rem] flex flex-col gap-5">
+                <h1 className="font-medium text-center text-3xl tracking-wider">Login</h1>
+                <LoginForm />
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default page
