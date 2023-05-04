6ec850f03e03b30166daedb633197ffa2c53c2bf
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-03 21:28:42 -0600

    Add key to map component

--- app/(components)/customer-reviews/professional/ProfessionalReviewCards.tsx
+++ app/(components)/customer-reviews/professional/ProfessionalReviewCards.tsx
@@ -13,7 +13,7 @@ const ProfessionalReviewCards = ({ data }: { data: ProfessionalReviewCardProps[]
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
             {data.map((card) => {
-                return <ProfessionalReviewCard data={card} />
+                return <ProfessionalReviewCard key={`${card.name}-review-card`} data={card} />
             })}
         </div>
     )
