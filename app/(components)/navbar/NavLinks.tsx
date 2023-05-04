915a6265d7bf61e1284bdd5c8c75a1d8134e5344
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-03 21:22:35 -0600

    Add NavLinks.tsx

---
+++ app/(components)/navbar/NavLinks.tsx
@@ -0,0 +1,20 @@
+const NavLinks = () => {
+    return (
+        <ul className="hidden lg:flex w-full justify-center text-center font-base text-sm gap-8 tracking-widest">
+            <li className="hover:text-beige cursor-pointer">
+                SHOP
+            </li>
+            <li className="hover:text-beige cursor-pointer">
+                OUR STORY
+            </li>
+            <li className="hover:text-beige cursor-pointer">
+                REVIEWS
+            </li>
+            <li className="hover:text-beige cursor-pointer">
+                REWARDS
+            </li>
+        </ul>
+    )
+}
+
+export default NavLinks
