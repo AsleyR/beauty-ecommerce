8ae3aaa8a49c7e0d8681e493b29da8fd69147447
AsleyR <79556058+AsleyR@users.noreply.github.com>
2023-05-06 12:39:04 -0600

    Add SearchIcon to MobileNavLinks

--- app/(components)/navbar/MobileNavLinks.tsx
+++ app/(components)/navbar/MobileNavLinks.tsx
@@ -1,23 +1,29 @@
 "use client"
 
-import { MobileSideMenuType } from "@/app/(libs)/context/MobileSideMenuContext"
+import { MobileSideMenuType } from "@/app/(context)/MobileSideMenuContext"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useContext, useState } from "react"
 import { MobileSideMenuContext } from "./Navbar"
+import SearchIcon from "../icons/SearchIcon"
+import Link from "next/link"
 
 const MobileNavLinks = () => {
     const { mobileSideMenu, setMobileSideMenu } = useContext(MobileSideMenuContext) as MobileSideMenuType
 
     return (
         <>
-            <div className="flex lg:hidden">
+            <div className="flex gap-5 lg:hidden">
                 <button onClick={() => setMobileSideMenu(!mobileSideMenu)}
                     className="flex hover:text-graytext">
                     <FontAwesomeIcon
                         className="w-[1.5rem] h-[1.5rem]"
                         icon={faBars} />
                 </button>
+                <Link href={'/'}
+                    className="flex hover:text-graytext">
+                    <SearchIcon className="" />
+                </Link>
             </div>
         </>
     )
